56d8c1c604f3d5fab6545d770d9ebf9553d1a60e
trsaund <tms.saunders@gmail.com>
2021-05-10 14:10:08 -0500

    Add files via upload

---
+++ VaccineRegistration/src/Vaccine.java
@@ -0,0 +1,24 @@
+public class Vaccine {
+
+    public String vaccineName;
+    private int numberOfDoses;
+    private int vaccineQuantity;
+
+    public String getName() {
+        return vaccineName;
+    }
+
+    public String getNumberOfDoses() {
+        return numberOfDoses;
+    }
+
+    public int getQuantity() {
+        return vaccineQuantity;
+    }
+
+    public void setQuantity(int newQuantity) {
+        vaccineQuantity = Math.max(newQuantity, 0); //if negative number is entered, 0 will be stored.
+
+    }
+
+}
---
+++ VaccineRegistration/src/VaccineSite.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class VaccineSite {
+    private String siteName;
+    private String siteAddress;
+    private String siteZipCode;
+    private String siteCity;
+    private String siteState;
+
+
+}
