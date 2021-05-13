175c6865669203070aeb85ace1f9a154f10a0efc
trsaund <tms.saunders@gmail.com>
2021-05-12 21:02:41 -0500

    updated NoAvailabilityPanel

--- VaccineRegistration/src/NoAvailabilityPanel.java
+++ VaccineRegistration/src/NoAvailabilityPanel.java
@@ -4,19 +4,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 
-public class NoAvailabilityPanel {
+public class NoAvailabilityPanel extends JPanel {
     private RegistrationGUI frame;
     private JLabel noAvailMessage;
 
     public NoAvailabilityPanel(RegistrationGUI frame) {
-        frame.setFocusable();
+        setFocusable(true);
         this.frame = frame;
-        frame.setLayout(new BorderLayout());
-        noAvailMessage = new JLabel("There are no vaccines available in your area at this time. " +
-                "Please check back again soon.", SwingConstants.CENTER);
+        setLayout(new BorderLayout());
+        noAvailMessage = new JLabel("Based on your answer to the eligibility " +
+                "questionaire, you are ineligible for the COVID-19 vaccine.", SwingConstants.CENTER);
         noAvailMessage.setFont(new Font("Sans Serif", Font.BOLD, 15));
         noAvailMessage.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        frame.add(noAvailMessage, BorderLayout.CENTER);
+        add(noAvailMessage, BorderLayout.CENTER);
+        setOpaque(false);
 
     }
+
 }
