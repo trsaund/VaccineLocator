26eac9dadd1df332b59b7a0e01f3886f4bb4c9d3
trsaund <tms.saunders@gmail.com>
2021-05-13 10:19:10 -0500

    fixed bug uploading txt file for CountySiteRoster

--- VaccineRegistration/src/VaccineSite.java
+++ VaccineRegistration/src/VaccineSite.java
@@ -19,10 +19,10 @@ public class VaccineSite {
         siteAddress = line.next();
         siteCity = line.next();
         siteState = line.next();
-        siteZipCode = line.nextDouble();
-        modernaCount = line.nextInt();
-        jandjCount = line.nextInt();
-        pfizerCount = line.nextInt();
+        //siteZipCode = line.nextDouble();
+        //modernaCount = line.nextInt();
+        //jandjCount = line.nextInt();
+       // pfizerCount = line.nextInt();
         /*This code will throw an error if text file with Site Info is formatted wrong, but we assume a file
         with any additional site info will be formatted correctly and input by programmers on the backend. */
 
